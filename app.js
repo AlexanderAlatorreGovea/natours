11ec298c611f565f25eccebaa82e0dcbb9e29b37
Alexander Govea <alexander_alatorre1993@hotmail.com>
2019-09-22 18:26:16 -0700

    added cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const compression = require('compression');
-//const cors = require('cors');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -31,6 +31,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 // 1)GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+
+app.options('*', cors());
+
 // Set security HTTP headers
 app.use(helmet());
 
